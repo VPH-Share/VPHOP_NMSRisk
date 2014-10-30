e5f652fb94306973f590ee5153c7d9fb5cb0934d
dtesti <d.testi@scsitaly.com>
2014-10-30 14:23:53 +0100

    updated to new script

--- manage/rollback.sh
+++ manage/rollback.sh
@@ -9,11 +9,11 @@ log "Updating OS packages"
 pkgupdate
 #######################################
 log "Stopping application"
-sudo service vphop_nms2ans stop
+sudo service vphop_nmsrisk stop
 #######################################
 log "Deconfigure SOAPLib to autostart"
-sudo update-rc.d vphop_nms2ans disable
-sudo rm /etc/init.d/vphop_nms2ans
+sudo update-rc.d vphop_nmsrisk disable
+sudo rm /etc/init.d/vphop_nmsrisk
 #######################################
 log "Uninstalling SOAPlib Commandline Wrapper dependencies"
 sudo pip uninstall -r $REPO_DIR/manage/requirements.txt
